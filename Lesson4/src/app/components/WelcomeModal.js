b0c3988a0d8d2ab9fad32b216b242b3d2ad0890d
NatalyaDol <suhoikova@gmail.com>
2019-06-19 13:32:54 +0300

    Added the WelcomeModal's component

--- Lesson4/src/app/components/WelcomeModal.js
+++ Lesson4/src/app/components/WelcomeModal.js
@@ -10,12 +10,31 @@ class WelcomeModal extends React.Component {
         console.log("WelcomeModal start 1 "); 
     }
 
+    //закрыть окно modal
+    removeModal() {
+        console.log("removeModal");        
+        this.setState({ display: !this.state.display })
+    };
     render() {
-        console.log("WelcomeModal render () 2  ");
+        console.log("WelcomeModal render () 2 ");
+        let visibleModal;
+        if (this.state.display) {
+            visibleModal = 
+            <section className="modal-window">
+                <div className="modal-window__block" id="my_modal">
+                <div className="modal-window__block__content">
+                    <span className="modal-window__block__content_cross" onClick={this.removeModal.bind(this)}>&times;</span>
+                    <p>Приветик</p>
+                </div>
+                </div>
+            </section>;
+        }
+
         return (
-          <div>
-             это WelcomeModal
-          </div>);
+            <div>
+                { visibleModal };
+            </div>
+        )
     }
     //Метод монтирования (react встроенный)  componentDidMount() вызывается сразу 
     //после монтирования (то есть, вставки компонента в DOM). 
@@ -23,6 +42,10 @@ class WelcomeModal extends React.Component {
     //выполняет побочные задачи(ajax запросы например)
     componentDidMount () {
         console.log("componentDidMount() 3");
+        console.log();
+        
+        this.setState({ display: !this.state.display })
+        
     }
 }
 
